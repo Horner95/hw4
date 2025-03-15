55d241a3e42639283e3330812d6b187b36d03f98
Will Horner <will.horner@kellogg.northwestern.edu>
2025-03-15 18:48:45 +0000

    adding images pt1

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,26 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if session["user_id"] == nil
+      flash["notice"] = "You must be logged in to create an entry."
+      redirect_to "/login"
+    else
+      @entry = Entry.new({ 
+        "title" => params["entry"]["title"], 
+        "description" => params["entry"]["description"], 
+        "occurred_on" => params["entry"]["occurred_on"], 
+        "place_id" => params["entry"]["place_id"], 
+        "user_id" => session["user_id"], 
+        "image_url" => params["entry"]["image_url"]  # ✅ Now correctly saves image URL
+      })
+
+      if @entry.save
+        flash["notice"] = "New entry created!"
+        redirect_to "/places/#{@entry.place_id}"
+      else
+        render "new"
+      end
+    end
   end
 
 end
